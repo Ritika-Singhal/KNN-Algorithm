213a85c2481b1ae397ab2e5dbda46d42c115a8a6
Ritika-Singhal <36181425+Ritika-Singhal@users.noreply.github.com>
2019-09-23 23:09:01 -0700

    Update knn.py

--- knn.py
+++ knn.py
@@ -1,7 +1,6 @@
 import numpy as np
 from collections import Counter
 
-
 class KNN:
     def __init__(self, k, distance_function):
         """
@@ -11,30 +10,22 @@ class KNN:
         self.k = k
         self.distance_function = distance_function
 
-    # TODO: save features and lable to self
     def train(self, features, labels):
         """
         In this function, features is simply training data which is a 2D list with float values.
-        For example, if the data looks like the following: Student 1 with features age 25, grade 3.8 and labeled as 0,
-        Student 2 with features age 22, grade 3.0 and labeled as 1, then the feature data would be
-        [ [25.0, 3.8], [22.0,3.0] ] and the corresponding label would be [0,1]
-
-        For KNN, the training process is just loading of training data. Thus, all you need to do in this function
-        is create some local variable in KNN class to store this data so you can use the data in later process.
         :param features: List[List[float]]
         :param labels: List[int]
         """
         self.train_features = features
         self.train_labels = labels
 
-    # TODO: predict labels of a list of points
     def predict(self, features):
         """
-        This function takes 2D list of test data points, similar to those from train function. Here, you need process
+        This function takes 2D list of test data points, similar to those from train function. Here, I process
         every test data point, reuse the get_k_neighbours function to find the nearest k neighbours for each test
         data point, find the majority of labels for these neighbours as the predict label for that testing data point.
-        Thus, you will get N predicted label for N test data point.
-        This function need to return a list of predicted labels for all test data points.
+        Thus, got N predicted label for N test data point.
+        This function returns a list of predicted labels for all test data points.
         :param features: List[List[float]]
         :return: List[int]
         """
@@ -46,12 +37,11 @@ class KNN:
             test_labels.append(majority_label)
         return test_labels
 
-    # TODO: find KNN of one point
     def get_k_neighbors(self, point):
         """
         This function takes one single data point and finds k-nearest neighbours in the training set.
-        You already have your k value, distance function and you just stored all training data in KNN class with the
-        train function. This function needs to return a list of labels of all k neighours.
+        We already have the k value, distance function and stored all training data in KNN class with the
+        train function. This function returns a list of labels of all k neighours.
         :param point: List[float]
         :return:  List[int]
         """
